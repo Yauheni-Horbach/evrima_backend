c0f5ce36b8c8c811ef205506703e79bc0b318d27
YauheniHorbach <horbachyauhenipetrovich@gmail.com>
2023-08-06 22:38:52 +0200

    Update user schema x2

--- src/common/schemas/user.schema.ts
+++ src/common/schemas/user.schema.ts
@@ -1,5 +1,90 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { TravelItem } from '../../types';
+
+const iconSchema = new mongoose.Schema({
+  prefix: String,
+  suffix: String,
+});
+
+const categorySchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  icon: iconSchema,
+});
+
+const geocodeSchema = new mongoose.Schema({
+  main: {
+    latitude: Number,
+    longitude: Number,
+  },
+});
+
+const hoursSchema = new mongoose.Schema({
+  is_local_holiday: Boolean,
+  open_now: Boolean,
+});
+
+const locationSchema = new mongoose.Schema({
+  address: String,
+  country: String,
+  formatted_address: String,
+  locality: String,
+  postcode: String,
+  region: String,
+});
+
+const photoSchema = new mongoose.Schema({
+  id: String,
+  created_at: String,
+  prefix: String,
+  suffix: String,
+  width: Number,
+  height: Number,
+});
+
+const placeItemSchema = new mongoose.Schema({
+  fsq_id: String,
+  categories: [categorySchema],
+  geocodes: geocodeSchema,
+  hours: hoursSchema,
+  location: locationSchema,
+  name: String,
+  photos: [photoSchema],
+  rating: Number,
+  website: String,
+  description: String,
+});
+
+const travelListSchema = new mongoose.Schema({
+  readonly: {
+    travelList: {
+      type: Map,
+      of: new mongoose.Schema({
+        id: String,
+        name: String,
+        coordinates: {
+          lat: Number,
+          lng: Number,
+        },
+        location: String,
+        radius: Number,
+        likeList: [placeItemSchema],
+        dislikeList: [placeItemSchema],
+        nextPageLink: String,
+        startDate: String,
+        endDate: String,
+        currentCoordinates: {
+          lat: Number,
+          lng: Number,
+        },
+        visitedPlaces: [String],
+      }),
+    },
+  },
+});
+
+const TravelListModel = mongoose.model('TravelList', travelListSchema);
 
 @Schema({
   timestamps: true,
@@ -47,7 +132,7 @@ export class User {
   @Prop({ default: '' })
   readonly currentTravelId: string;
 
-  @Prop({ default: {} })
+  @Prop({ default: {}, type: TravelListModel })
   readonly travelList: {
     [key: string]: TravelItem;
   };
